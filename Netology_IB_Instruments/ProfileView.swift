578f86c1c50761875c27a690cc2cc18a6cd7db0a
Ser2063 <mesoil2063@gmail.com>
2022-03-15 21:47:58 +0300

    Storyboard added and configured

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Sergey RZHEVSKIY on 15.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+
+    @IBOutlet weak var myImageView: UIImageView!
+
+    @IBOutlet weak var myLabelName: UILabel!
+
+    @IBOutlet weak var myLabelDate: UILabel!
+
+    @IBOutlet weak var myLabelCity: UILabel!
+
+    @IBOutlet weak var myTextView: UITextView!
+}
+
+
+
